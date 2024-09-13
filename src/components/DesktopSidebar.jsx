3f6643950754f81c8f26b1bc97c61fad80b5e756
sacha11 <s.j.g_97@hotmail.com>
2024-09-13 16:51:23 -0300

    title updated and default images set up

--- src/components/DesktopSidebar.jsx
+++ src/components/DesktopSidebar.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 function DesktopSidebar() {
   return (
-    <div>
-        <div>
+    <div className='p-3 md:p-10 border-r min-h-screen w-24 md:w-64 hidden sm:block'>
+        <div className='flex flex-col gap-20 sticky top-10 left-0'>
             
         </div>
     </div>
