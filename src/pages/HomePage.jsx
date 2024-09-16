ebdc12cd684d4a062799ede434f4ca19ab6f4ecb
sacha11 <s.j.g_97@hotmail.com>
2024-09-16 12:53:45 -0300

    recipe component in dev

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,4 +1,4 @@
-import { Search } from 'lucide-react'
+import { Heart, Search } from 'lucide-react'
 import React from 'react'
 
 function HomePage() {
@@ -20,7 +20,25 @@ function HomePage() {
           Popular Choices
         </p>
         <div id='recipe grid' className='grid gap-3 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
-          
+          {/* first recipe */}
+          <div className='flex flex-col rounded-md bg-[#ecf7d4] overflow-hidden p-3 relative'>
+            <a href="" className='relative h-32'>
+              <img src="/1.jpg" alt="recipe img" className='rounded-md w-full h-full object-cover cursor-pointer' />
+
+              <div className='absolute bottom-2 left-2 bg-white rounded-full p-1 cursor-pointer flex items-center gap-1 text-sm'>
+                <Soup size={16}/> 4 Servings
+              </div>
+
+              <div className='absolute top-1 right-2 bg-white rounded-full p-1 cursor-pointer'>
+                <Heart size={20} className='hover:fill-red-500 hover:text-red-500' />
+              </div>
+
+            </a>
+            <div className='flex mt-1'>
+              <p className='font-bold tracking-wide'>Roasted Chicken</p>
+            </div>
+
+          </div>
         </div>
       </div>
     </div>
